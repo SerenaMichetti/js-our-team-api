1feeecf5977eef92e8ba549efa7dae15a3129ba0
Serena <sere.michetti@icloud.com>
2026-01-09 10:30:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const teamMembers = [
+];
+
+const apiUrl= 'https://boolean-teachers.github.io/mock/api/members/'
+//richiamo la card html
+
+function renderCards(dati){
+
+  const elementCard = document.getElementById('cards-container')
+  
+  let htmlCards = ''
+  
+  for (const teamMember of teamMembers) {
+    let card = `<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${teamMember.img}" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">${teamMember.name}</h5>
+    <p class="card-text">${teamMember.role}</p>
+    <a href="mailto:EMAIL">${teamMember.email}</a>
+    </div>
+    </div>`
+    
+    htmlCards += card
+  }
+  
+  elementCard.innerHTML = htmlCards
+  
+}
+
+axios.get(apiUrl).then(response =>{
+  renderCards(response.data);
+}).catch(err=>){
+  
+}
